d6d3072ede0a847b4ceec4f8f9ede953147026c7
Mathias Munk Hansen <matmunk@gmail.com>
2014-03-14 00:24:21 +0100

    Wrapped everything in a Glicko class.

--- glicko/glicko.py
+++ glicko/glicko.py
@@ -1,26 +1,46 @@
+"""
+An implementation of the Glicko system.
+
+Calculates Glicko ratings and RDs for single-game rating periods.
+The technical details of the Glicko system are described in:
+http://www.glicko.net/glicko/glicko.pdf
+"""
+
 import math
 
-c = 17.5
-q = math.log(10) / 400
+class Glicko:
 
-def g(rd):
-	return 1 / math.sqrt(1 + 3 * q ** 2 * rd ** 2 / math.pi ** 2)
+    """
+    unranked_rd - The default RD assigned to unranked players (default 350).
+    min_rd - The lowest possible RD of a player (default 30).
+    c - Constant which governs the increase in uncertainty over time.
+    """
 
-def E(rating, rating_opp, rd_opp):
-	return 1 / (1 + 10 ** (-g(rd_opp) * (rating - rating_opp) / 400))
+    def __init__(self, c, unranked_rd=350, min_rd=30):
+        self.unranked_rd = unranked_rd
+        self.min_rd = min_rd
+        self.c = c
+        self.q = math.log(10) / 400
 
-def d(rating, rating_opp, rd_opp):
-	valE = E(rating, rating_opp, rd_opp)
-	return 1 / (q ** 2 * g(rd_opp) ** 2 * valE * (1 - valE))
+    def _g(self, rd):
+        return 1 / math.sqrt(1 + 3 * self.q**2 * rd**2 / math.pi**2)
 
-def update_rd(rd_old, t):
-	return min(math.sqrt(rd_old ** 2 + c ** 2 * t), 350)
+    def _E(self, rating, rating_opp, rd_opp):
+        return 1 / (1 + 10**(-self._g(rd_opp) * (rating - rating_opp) / 400))
 
-def update_player(rating, rd, rating_opp, rd_opp, t, outcome):
-	rd = update_rd(rd, t)
-	valD = d(rating, rating_opp, rd_opp)
+    def _d(self, rating, rating_opp, rd_opp):
+        valE = self._E(rating, rating_opp, rd_opp)
+        return 1 / (self.q**2 * self._g(rd_opp)**2 * valE * (1 - valE))
 
-	new_rating = rating + q / (1 / rd ** 2 + 1 / valD) * g(rd_opp) * (outcome - E(rating, rating_opp, rd_opp))
-	new_rd = math.sqrt(1 / (1 / rd ** 2 + 1 / valD))
+    def _update_rd(self, rd_old, t):
+        return min(math.sqrt(rd_old**2 + self.c**2 * t), self.unranked_rd)
 
-	return (int(new_rating), int(new_rd))
+    def update_player(self, rating, rd, rating_opp, rd_opp, t, outcome):
+        """Return the updated rating and RD of a player."""
+        rd = self._update_rd(rd, t)
+        valD = self._d(rating, rating_opp, rd_opp)
+
+        new_rating = rating + self.q / (1 / rd**2 + 1 / valD) * self._g(rd_opp) * (outcome - self._E(rating, rating_opp, rd_opp))
+        new_rd = math.sqrt(1 / (1 / rd**2 + 1 / valD))
+
+        return (int(new_rating), max(int(new_rd), self.min_rd))
