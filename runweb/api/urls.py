ca14bf8ade6ba60f0dc68c57ee1488bb9beb90f9
Jeppe B. Tarp <raider.j@gmail.com>
2014-03-23 18:43:33 +0100

    Allow the API to grab a CSRF token

--- runweb/api/urls.py
+++ runweb/api/urls.py
@@ -3,4 +3,5 @@ from django.conf.urls import patterns, include, url
 urlpatterns = patterns('',
     url(r'^login/$', 'runweb.views.session_login', {'as_json': True}),
     url(r'^routes/$', 'routes.views.overview', {'as_json': True}),
+    url(r'^init/$', 'api.views.init'),
 )
