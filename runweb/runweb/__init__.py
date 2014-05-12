e75037d92bb00b23395db09ba249e7f00c155df2
Jeppe B. Tarp <raider.j@gmail.com>
2014-05-12 22:33:26 +0200

    Typo

--- runweb/runweb/__init__.py
+++ runweb/runweb/__init__.py
@@ -2,4 +2,4 @@ try:
     import pymysql
     pymysql.install_as_MySQLdb()
 except ImportError:
-    print("PyMQL not available, use another database engine")
+    print("PyMySQL not available, use another database engine")
