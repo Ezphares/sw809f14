2e416597395a8282c0f1ec1c3669cb892cc7102d
DorkMatter <barbaraalbertine@gmail.com>
2014-05-17 00:36:19 +0200

    added login prompt

--- RunApp/src/dk/aau/student/runapp/UserInfo.java
+++ RunApp/src/dk/aau/student/runapp/UserInfo.java
@@ -5,13 +5,13 @@ public class UserInfo
 	static String username = null;
 	static String password = null;
 	static int id;
-	private static UserInfo userinfo = new UserInfo();
+	private static UserInfo userinfo = null;
 	
 	private UserInfo()
 	{
-		username = "test";
-		password = "123456";
-		id = 2;		
+		this.username = "";
+		this.password = "";
+		this.id = -1;		
 	}
 	
 	static String get_username()
@@ -27,5 +27,30 @@ public class UserInfo
 	static int get_id()
 	{
 		return id;
+	}
+	
+	public void set_username(String username)
+	{
+		this.username = username;
+	}
+	
+	public void set_password(String password)
+	{
+		this.password = password;
+	}
+	
+	public void set_id(int id)
+	{
+		this.id = id;
+	}
+	
+	public static UserInfo get_instance()
+	{
+		if(userinfo == null)
+		{
+			userinfo = new UserInfo();
+		}
+		
+		return userinfo;
 	}
 }
